7ff89a43ec39cba25f2f08ce86781b42de246fad
PhenoGeno <82991162+PhenoGeno@users.noreply.github.com>
2021-04-22 08:01:30 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Grabbing html ids and initiating variables
-let addToDoButton = document.getElementById('sub');
+let submit = document.getElementById('sub');
 let toDoContainer = document.getElementById('toDoContainer');
 let inputField = document.getElementById('inputField');
 let counter = document.getElementById('counter')
@@ -8,24 +8,28 @@ let number = 0
 
 // Defining the main function which creates the 'to-does' and also calls upon the tCounter function to keep track of the tasks completed
 function all (){
-    var paragraph = document.createElement('p');
-    paragraph.classList.add('paragraph-styling-' + urgency);
-    paragraph.innerText = inputField.value;
-    toDoContainer.appendChild(paragraph);
-    inputField.value = ""
-    paragraph.addEventListener('click', function(){
-        paragraph.style.textDecoration = "line-through";
-    });
-    paragraph.addEventListener('dblclick', function(){
-        toDoContainer.removeChild(paragraph);
-        tCounter();
-        playSound();
-    });
+        var paragraph = document.createElement('p');
+        paragraph.classList.add('paragraph-styling-' + urgency);
+        paragraph.innerText = inputField.value;
+        toDoContainer.appendChild(paragraph);
+        inputField.value = ""
+        paragraph.addEventListener('click', function(){
+            paragraph.style.textDecoration = "line-through";
+        });
+        paragraph.addEventListener('dblclick', function(){
+            toDoContainer.removeChild(paragraph);
+            tCounter();
+            playSound();
+        });
+    
 }
 
 // Defining the function that separates the different colored urgencies and calls upon the 'all' function accordingly
 function init (){
-     urgency = Number(prompt("How urgent is this task on a scale of 1-3?"));
+    if (inputField.value === ''){
+        alert('You must enter a task!');
+    } else {
+        urgency = Number(prompt("How urgent is this task on a scale of 1-3?"));
         if(isNaN(urgency)) {
             alert("The urgency must be a number");
             return null;
@@ -43,6 +47,7 @@ function init (){
             all();
         } else {
             return null
+        }
     }
 };
 
@@ -52,8 +57,15 @@ function tCounter (){
     counter.innerText = 'Total Tasks Completed - ' + number
 }
 
+// Defining the function that will play a quick beep
+function playSound() {
+    var sound = document.getElementById("audio");
+    sound.volume = .2;
+    sound.play();
+}
+
 // Event listeners that execute the init function when the submit button or enter key are pressed
-addToDoButton.addEventListener('click', function(){
+submit.addEventListener('click', function(){
     init();
 });
 inputField.addEventListener("keypress", function(e) {
@@ -62,9 +74,16 @@ inputField.addEventListener("keypress", function(e) {
     }
 });
 
-// Defining the function that will play a quick beep
-function playSound() {
-    var sound = document.getElementById("audio");
-    sound.volume = .2;
-    sound.play();
-}
+var paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling-' + urgency);
+    paragraph.innerText = inputField.value;
+    toDoContainer.appendChild(paragraph);
+    inputField.value = ""
+    paragraph.addEventListener('click', function(){
+        paragraph.style.textDecoration = "line-through";
+    });
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+        tCounter();
+        playSound();
+    });
